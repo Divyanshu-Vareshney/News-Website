319a0dabee2854b9c34d8243c1bc0d6f706fc461
Divyanshu-Vareshney <varshneydivyanshu2904@gmail.com>
2022-11-05 19:43:59 +0530

    javascript

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // api key c202163307084196ab5947625405dbdc
+//variables
+const generalBtn = document.getElementById("genral");
+const businessBtn = document.getElementById("business");
+const sportsBtn = document.getElementById("sport");
+const entertainmentBtn = document.getElementById("entertainment");
+const technologyBtn = document.getElementById("technology");
+const searchBtn = document.getElementById("searchBtn");
+
+//apis
